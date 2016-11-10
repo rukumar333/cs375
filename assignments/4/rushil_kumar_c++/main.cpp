11217b0df12d15795b874c87e74dd27180a90c16
Rushil <rukumar333@gmail.com>
2016-11-09 23:44:30 -0500

    Saving.

--- assignments/4/rushil_kumar_c++/main.cpp
+++ assignments/4/rushil_kumar_c++/main.cpp
@@ -4,6 +4,10 @@
 #include <sstream>
 #include "Graph.h"
 
+double linearProgramming();
+double simplexMethod(std::vector<std::vector<double > > &matrix);
+void printMatrix(std::vector<std::vector<double > > &matrix);
+
 int main(){
     // std::cout << std::stoi("cs15") << std::endl;
     std::ifstream file("input.txt");
@@ -21,4 +25,79 @@ int main(){
     }
     Graph graph = Graph(matrix);
     std::cout << "Minimum number of semesters: " << graph.findMinSemesters() << std::endl;
+    std::cout << "Cents: " << linearProgramming() << std::endl;
+}
+
+double linearProgramming(){
+    std::vector<std::vector<double > > matrix = {
+	{60, 60, 1, 0, 0, 300},
+	{12, 6, 0, 1, 0, 36},
+	{10, 30, 0, 0, 1, 90},
+	{-12, -15, 0, 0, 0, 0}
+    };
+    return simplexMethod(matrix);
+}
+
+double extraCredit(){
+    // std::vector<std::vector<double> > matrix = {
+    // 	{}
+    // }
+}
+
+double simplexMethod(std::vector<std::vector<double > > &matrix){
+    int x = matrix.size();
+    int y = matrix[0].size();
+    int pivotColumn = -1;
+    // int length = matrix[matrix.size() - 1];
+    for(int i = 0; i < y; ++ i){
+	if(pivotColumn != -1 && matrix[x - 1][i] < matrix[x - 1][pivotColumn]){
+	    pivotColumn = i;
+	}else{
+	    if(pivotColumn == -1 && matrix[x - 1][i] < 0)
+		pivotColumn = i;
+	}
+    }
+    if(pivotColumn == -1){
+	return matrix[x - 1][y - 1];
+    }
+    int pivotRow = -1;
+    double dividedNum = 0;
+    for(int i = 0; i < x - 1; ++ i){
+	double tempNum = matrix[i][y - 1] / matrix[i][pivotColumn];
+	if(pivotRow == -1 && tempNum > 0){
+	    dividedNum = tempNum;
+	    pivotRow = i;
+	}else{
+	    if(dividedNum > tempNum && tempNum > 0){
+		dividedNum = tempNum;
+		pivotRow = i;
+	    }
+	}
+    }
+    //Divide by pivot element
+    double pivotElement = matrix[pivotRow][pivotColumn];
+    for(int i = 0; i < y; ++ i){
+	matrix[pivotRow][i] = matrix[pivotRow][i] / pivotElement;
+    }    
+    //Cancel other rows out
+    for(int i = 0; i < x; ++ i){
+	if(i != pivotRow){
+	    double mult = matrix[i][pivotColumn];
+	    for(int j = 0; j < y; ++ j){
+		matrix[i][j] = matrix[i][j] - (mult * matrix[pivotRow][j]);
+	    }
+	}
+    }
+    // std::cout << pivotColumn << std::endl;
+    // std::cout << pivotRow << std::endl;
+    return simplexMethod(matrix);
+}
+
+void printMatrix(std::vector<std::vector<double > > &matrix){
+    for(int i = 0; i < matrix.size(); ++ i){
+	for(int j = 0; j < matrix[i].size(); ++ j){
+	    std::cout << matrix[i][j] << " ";
+	}
+	std::cout << std::endl;
+    }
 }
